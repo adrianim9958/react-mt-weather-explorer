ff60f6cc1606318e101f91dbdc40c47b6e72caae
akabong <akabongee@gmail.com>
2025-11-06 08:22:30 +0900

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import 'leaflet/dist/leaflet.css';
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode><App /></React.StrictMode>
+);
